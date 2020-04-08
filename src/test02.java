18eb53166bb6b8f41013db9f9ac44e9f05e0ebe5
Taek <Taek@DESKTOP-GV52NF9>
2020-04-09 00:23:30 +0900

    commit test

---
+++ src/test02.java
@@ -0,0 +1,17 @@
+
+public class test02 {
+	public static void main(String[] args){
+		
+		int a,c;
+		a=2;
+		c=3;
+		
+		for(int b=1; b < 10; b++) {
+			System.out.print(a + "*"+ b + "="  + a*b);
+			System.out.print("     ");
+			System.out.println(c + "*" + b + "=" + c*b);
+		}
+		
+		
+	}
+}
